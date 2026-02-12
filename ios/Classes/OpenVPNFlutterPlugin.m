0370c325957f1fe72451502ce89f8eda32439d84
Arslan Farooq <Arsl143@yahoo.com>
2026-02-12 09:09:23 +0300

    fix: Update iOS imports to use axevpn_flutter instead of openvpn_flutter
    - Changed all import statements in OpenVPNFlutterPlugin.m
    - Now imports axevpn_flutter-Swift.h instead of openvpn_flutter-Swift.h
    - Matches the renamed podspec and plugin name

--- ios/Classes/OpenVPNFlutterPlugin.m
+++ ios/Classes/OpenVPNFlutterPlugin.m
@@ -1,11 +1,11 @@
 #import "OpenvpnFlutterPlugin.h"
-#if __has_include(<openvpn_flutter/openvpn_flutter-Swift.h>)
-#import <openvpn_flutter/openvpn_flutter-Swift.h>
+#if __has_include(<axevpn_flutter/axevpn_flutter-Swift.h>)
+#import <axevpn_flutter/axevpn_flutter-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "openvpn_flutter-Swift.h"
+#import "axevpn_flutter-Swift.h"
 #endif
 
 @implementation OpenVPNFlutterPlugin
